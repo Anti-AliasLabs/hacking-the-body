27008734a1fd0fb24b63561d8f06b53907664abb
theleadingzero <becky@theleadingzero.com>
2016-02-06 08:51:02 +0000

    Arduino sketch with MPR121.

---
+++ mqtt_esp8266/mqtt_esp8266.ino
@@ -0,0 +1,312 @@
+/***************************************************
+ * 
+ * Written by Becky Stewart
+ * Feb 2016
+ * 
+ * Adapted from:
+  Adafruit MQTT Library ESP8266 Example
+
+  Must use ESP8266 Arduino from:
+    https://github.com/esp8266/Arduino
+
+  Works great with Adafruit's Huzzah ESP board:
+  ----> https://www.adafruit.com/product/2471
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Tony DiCola for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#include "mpr121.h"
+#include <Wire.h>
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "AWR-7200"     //"Up n Jammin" //"whereismycoffee"
+#define WLAN_PASS       "hackthebody"  //"moscowdisco" //"x3zsgb4z"
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "192.168.1.5"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    ""
+#define AIO_KEY         ""
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+// Store the MQTT server, username, and password in flash memory.
+// This is required for using the Adafruit MQTT library.
+const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
+const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, AIO_SERVERPORT, MQTT_USERNAME, MQTT_PASSWORD);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'touch' for publishing.
+// Notice MQTT paths: <username>/feeds/<feedname>
+const char TOUCH_FEED[] PROGMEM = AIO_USERNAME "/feeds/shoulder/touch";
+Adafruit_MQTT_Publish touch = Adafruit_MQTT_Publish(&mqtt, TOUCH_FEED);
+
+// Setup a feed called 'motor' for subscribing to changes.
+const char MOTOR_FEED[] PROGMEM = AIO_USERNAME "/feeds/shoulder/motor";
+Adafruit_MQTT_Subscribe motor = Adafruit_MQTT_Subscribe(&mqtt, MOTOR_FEED);
+
+/*************************** Cap Sensing ************************************/
+int irqpin = 2;  // Digital 2
+boolean touchStates[12]; //to keep track of the previous touch states
+int motorPin = 12;
+
+/*************************** Sketch Code ************************************/
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println(F("---Hack the Body---"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  // Setup MQTT subscription for motor feed.
+  mqtt.subscribe(&motor);
+
+  // Set up MPR121
+  pinMode(irqpin, INPUT);
+  digitalWrite(irqpin, HIGH); //enable pullup resistor
+  Wire.begin();
+
+  mpr121_setup();
+
+  pinMode(motorPin, OUTPUT);
+  digitalWrite(motorPin, LOW);
+}
+
+uint32_t x = 0;
+
+void loop() {
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
+  // this is our 'wait for incoming subscription packets' busy subloop
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+    Serial.print(F("Got: "));
+    if (subscription == &motor) {
+      Serial.println((char *)motor.lastread);
+      // activate motor
+      triggerMotor();
+      //char* lastChar = (char *)onoffbutton.lastread;
+      /*Serial.println(lastChar);
+      if(lastChar == "0") {
+        digitalWrite(2, HIGH);
+        delay(500);
+        digitalWrite(2, LOW); 
+      }*/
+    }
+  }
+  readTouchInputs();
+
+  // ping the server to keep the mqtt connection alive
+  if (! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+
+  delay(50);
+}
+
+void readTouchInputs() {
+  if (!checkInterrupt()) {
+
+    //read the touch state from the MPR121
+    Wire.requestFrom(0x5A, 2);
+
+    byte LSB = Wire.read();
+    byte MSB = Wire.read();
+
+    uint16_t touched = ((MSB << 8) | LSB); //16bits that make up the touch states
+
+    int touchTriggered = 0;
+    for (int i = 0; i < 12; i++) { // Check what electrodes were pressed
+      if (touched & (1 << i)) {
+
+        if (touchStates[i] == 0) {
+          //pin i was just touched
+          Serial.print("pin ");
+          Serial.print(i);
+          Serial.println(" was just touched");
+          touchStates[i] = 1;
+
+          touchTriggered++;
+          //Serial.println(touchTriggered);
+          if (touchTriggered == 1) {
+            // activate motor
+            //triggerMotor();
+            //Serial.println("motor triggered");
+            if (! touch.publish(i)) {
+              Serial.println(F("Failed"));
+            } else {
+              Serial.println(F("OK!"));
+            }
+          }
+        } else if (touchStates[i] == 1) {
+          //pin i is still being touched
+          Serial.print("pin ");
+          Serial.print(i);
+          Serial.println(" still being touched");
+        }
+
+
+      } else {
+        if (touchStates[i] == 1) {
+          Serial.print("pin ");
+          Serial.print(i);
+          Serial.println(" is no longer being touched");
+
+          //pin i is no longer being touched
+          touchStates[i] = 0;
+        }
+      }
+    }
+  }
+}
+
+void triggerMotor() {
+  digitalWrite(motorPin, HIGH);
+  Serial.println("motor on");
+  delay(300);
+  digitalWrite(motorPin, LOW);
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+  }
+  Serial.println("MQTT Connected!");
+}
+
+void mpr121_setup(void) {
+
+  set_register(0x5A, ELE_CFG, 0x00);
+
+  // Section A - Controls filtering when data is > baseline.
+  set_register(0x5A, MHD_R, 0x01);
+  set_register(0x5A, NHD_R, 0x01);
+  set_register(0x5A, NCL_R, 0x00);
+  set_register(0x5A, FDL_R, 0x00);
+
+  // Section B - Controls filtering when data is < baseline.
+  set_register(0x5A, MHD_F, 0x01);
+  set_register(0x5A, NHD_F, 0x01);
+  set_register(0x5A, NCL_F, 0xFF);
+  set_register(0x5A, FDL_F, 0x02);
+
+  // Section C - Sets touch and release thresholds for each electrode
+  set_register(0x5A, ELE0_T, TOU_THRESH);
+  set_register(0x5A, ELE0_R, REL_THRESH);
+
+  set_register(0x5A, ELE1_T, TOU_THRESH);
+  set_register(0x5A, ELE1_R, REL_THRESH);
+
+  set_register(0x5A, ELE2_T, TOU_THRESH);
+  set_register(0x5A, ELE2_R, REL_THRESH);
+
+  set_register(0x5A, ELE3_T, TOU_THRESH);
+  set_register(0x5A, ELE3_R, REL_THRESH);
+
+  set_register(0x5A, ELE4_T, TOU_THRESH);
+  set_register(0x5A, ELE4_R, REL_THRESH);
+
+  set_register(0x5A, ELE5_T, TOU_THRESH);
+  set_register(0x5A, ELE5_R, REL_THRESH);
+
+  set_register(0x5A, ELE6_T, TOU_THRESH);
+  set_register(0x5A, ELE6_R, REL_THRESH);
+
+  set_register(0x5A, ELE7_T, TOU_THRESH);
+  set_register(0x5A, ELE7_R, REL_THRESH);
+
+  set_register(0x5A, ELE8_T, TOU_THRESH);
+  set_register(0x5A, ELE8_R, REL_THRESH);
+
+  set_register(0x5A, ELE9_T, TOU_THRESH);
+  set_register(0x5A, ELE9_R, REL_THRESH);
+
+  set_register(0x5A, ELE10_T, TOU_THRESH);
+  set_register(0x5A, ELE10_R, REL_THRESH);
+
+  set_register(0x5A, ELE11_T, TOU_THRESH);
+  set_register(0x5A, ELE11_R, REL_THRESH);
+
+  // Section D
+  // Set the Filter Configuration
+  // Set ESI2
+  set_register(0x5A, FIL_CFG, 0x04);
+
+  // Section E
+  // Electrode Configuration
+  // Set ELE_CFG to 0x00 to return to standby mode
+  set_register(0x5A, ELE_CFG, 0x0C);  // Enables all 12 Electrodes
+
+
+  // Section F
+  // Enable Auto Config and auto Reconfig
+  /*set_register(0x5A, ATO_CFG0, 0x0B);
+  set_register(0x5A, ATO_CFGU, 0xC9);  // USL = (Vdd-0.7)/vdd*256 = 0xC9 @3.3V   set_register(0x5A, ATO_CFGL, 0x82);  // LSL = 0.65*USL = 0x82 @3.3V
+  set_register(0x5A, ATO_CFGT, 0xB5);*/  // Target = 0.9*USL = 0xB5 @3.3V
+
+  set_register(0x5A, ELE_CFG, 0x0C);
+
+}
+
+
+boolean checkInterrupt(void) {
+  return digitalRead(irqpin);
+}
+
+
+void set_register(int address, unsigned char r, unsigned char v) {
+  Wire.beginTransmission(address);
+  Wire.write(r);
+  Wire.write(v);
+  Wire.endTransmission();
+}
